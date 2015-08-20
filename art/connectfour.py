21c234072484b2a6b4d756f325dc1c47a154f7fc
DrSwag <drswag@mailinator.com>
2015-08-20 10:58:18 +0100

    Running art/connectfour.py

--- art/connectfour.py
+++ art/connectfour.py
@@ -4,13 +4,17 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 color("blue")
 box(50,50,725,725)
 
-x = 125
-y = 125
-color("white")
 
-for i in range(8):
-  for j in range(8):
-    spot(x,y,37.5)
-    x += 80
+def setgrid():
   x = 125
-  y += 80
+  y = 125
+  color("white")
+
+  for i in range(8):
+    for j in range(8):
+      spot(x,y,37.5)
+      x += 80
+    x = 125
+    y += 80
+    
+setgrid()
